8e0e98a426d3731a2ec01c3483c8ea61f03bb5ef
hosam <hosamelmalt@gmail.com>
2020-11-17 13:40:25 +0200

    Initial Commit

---
+++ Salad/SaladApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaladApp.swift
+//  Salad
+//
+//  Created by hosam on 11/17/20.
+//
+
+import SwiftUI
+
+@main
+struct SaladApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
